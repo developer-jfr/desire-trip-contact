6ac797bd368bc20549ec052bb4615f1337818a38
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-10-08 20:29:40 +0600

    refaactor code

--- assets/js/multi-select.js
+++ assets/js/multi-select.js
@@ -1,9 +1,13 @@
 
-$(".js-example-basic-multiple").select2({
+$(".country").select2({
     maximumSelectionLength: 3,
     placeholder: 'Where would you want to go?'
   });
 
+  //interest
 
 
- 
+  $(".interest").select2({
+    placeholder: 'Your interests?',
+    maximumSelectionLength: 4,
+  });
